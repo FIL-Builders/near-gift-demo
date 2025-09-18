2306fd7df9ed6cfd8c11ac557b341a69ac6dabef
Mikers <snissn@gmail.com>
2025-09-17 14:57:44 -1000

    fix(revert-a478f2d): redirect /account, /deposit, /withdraw to gift route to avoid missing feature code; build OK

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,33 +1,5 @@
-"use client"
-import { Suspense } from "react"
+import { redirect } from "next/navigation"
 
-import { AccountWidget } from "@src/components/DefuseSDK/features/account/components/AccountWidget"
-
-import Paper from "@src/components/Paper"
-import { LIST_TOKENS } from "@src/constants/tokens"
-import { useConnectWallet } from "@src/hooks/useConnectWallet"
-import { useTokenList } from "@src/hooks/useTokenList"
-import { renderAppLink } from "@src/utils/renderAppLink"
-
-function AccountContent() {
-  const { state } = useConnectWallet()
-  const tokenList = useTokenList(LIST_TOKENS)
-
-  return (
-    <Paper>
-      <AccountWidget
-        tokenList={tokenList}
-        userAddress={(state.isVerified ? state.address : undefined) ?? null}
-        userChainType={state.chainType ?? null}
-        renderHostAppLink={renderAppLink}
-      />
-    </Paper>
-  )
-}
 export default function AccountPage() {
-  return (
-    <Suspense fallback={null}>
-      <AccountContent />
-    </Suspense>
-  )
+  redirect("/gift-card/create-gift")
 }
